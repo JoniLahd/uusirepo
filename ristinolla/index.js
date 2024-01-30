02d7243644d7db7efccfef0a93d059344b614af6
JoniLahd <131016258+JoniLahd@users.noreply.github.com>
2024-01-30 23:10:05 +0200

    Add files via upload

---
+++ ristinolla/index.js
@@ -0,0 +1,73 @@
+//haetaan tarvittavat elementit html tiedostosta.
+const board = document.getElementById('board');
+const cells = Array.from(board.children);
+const restartBtn = document.getElementById('restart-btn');
+const status = document.getElementById('status');
+
+//Alustetaan pelitila, nykyinen pelaaja ja asetetaan EvenListenerit solujen(cell) klikkauksille ja uudelleenkäynnistyspainikkeelle(restartGame).
+let currentPlayer = 'X';
+let gameState = Array(9).fill(null);
+
+//Määritetään handleCellClick-funktio, joka käsittelee solun klikkauksia, päivittää pelitilan ja tarkistaa voiton tai tasapelin.
+function handleCellClick(event) {
+  const cellIndex = event.target.dataset.cellIndex;
+  if (gameState[cellIndex] !== null) return;
+  gameState[cellIndex] = currentPlayer;
+  event.target.textContent = currentPlayer;
+  checkForWin();
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  updateStatus();
+}
+
+//Määritetään checkForWin-funktio, joka tarkistaa voiton vertaamalla pelitilaa voittotilanteisiin.
+function checkForWin() {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  //Käydään läpi voittotilanteet
+  for (const condition of winConditions) {
+    //Haetaan pelitilan arvot voittoriviltä
+    const [a, b, c] = condition.map(index => gameState[index]);
+    //tarkistetaan onko voittorivillä kaikki arvot samat (ei-null)
+    if (a !== null && a === b && b === c) {
+        //näyttää voitto ilmoituksen pelaajalle ja päivittää tilan eli kuka voitti ja samalla käynnistää pelin uudelleen
+        alert(`Player ${currentPlayer} wins!`);
+        updateStatus();
+       
+        return; //keskeyttää funktion, koska peli päättyi
+    }
+  }
+  // Jos kaikki solut ovat täynnä, mutta voittoriviä ei löytynyt, kyseessä on tasapeli
+  if (!gameState.includes(null)) {
+    // Näytää tasapeli-ilmoitus ja päivitää tilan(updateStatus())
+    alert('It\'s a draw!');
+    updateStatus();
+   
+    return; //keskeyttää funktion, koska peli päättyi
+  }
+}
+
+//Määritetään restartGame-funktio, joka palauttaa pelitilan oletusarvoihin, tyhjentää laudan ja päivittää tilan.
+function restartGame() {
+  gameState = Array(9).fill(null);
+  cells.forEach(cell => cell.textContent = '');
+  currentPlayer = 'X';
+  updateStatus();
+}
+
+//Määritetään updateStatus-funktio, joka päivittää tilaelementin(eli kenen vuoro on) pelaajille
+function updateStatus() {
+  status.textContent = `It's ${currentPlayer}'s turn.`;
+}
+//Liitetään EventListenert soluihin ja restart painikkeeseen, jotka kutsuvat handleCellClick- ja restartGame-funktioita vastaavasti.
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+restartBtn.addEventListener('click', restartGame);
+updateStatus();
